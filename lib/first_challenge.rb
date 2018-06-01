e63259724a9222ebd9ab12fdf0433067b44c38d0
Allegra <aukseallegra@gmail.com>
2018-06-01 15:36:19 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,6 +16,7 @@ def first_challenge
   #your code here
 contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
 contacts["Freddy Mercury"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
+contacts["Freddy Mercury"][:favorite_icecream_flavors]
 
   #remember to return your newly altered contacts hash!
   contacts
